8271525c7edbd792348468391858e95c9f31b0fc
vivikikiy <101606936+vivikikiy@users.noreply.github.com>
2023-06-30 10:21:43 -0300

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021 Valentin Milea <valentin.milea@gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <dht.h>
+#include <pico/stdlib.h>
+#include <stdio.h>
+
+// DHT usado (cambiar por DHT22 si fuese el caso)
+static const dht_model_t DHT_MODEL = DHT11;
+// GPIO a usar para los datos
+static const uint DATA_PIN = 16;
+                                       mmmmmmm
+/*
+ * @brief Funcion que convierte de Celsius a Fahrenheit
+ * @param celsius: temperatura en grados Celsius
+ * @return temperatura en grados Fahrenheit
+ */
+static inline float celsius_to_fahrenheit(float celsius) {
+    // Retornar el valor de la temperatura en Fahrenheit
+    
+    return celsius *(9/5)+32;
+
+}
+
+/*
+ * @brief Programa principal
+ */
+int main() {
+    // Inicializacion de USB
+    stdio_init_all();
+    // Instancia de DHT
+    dht_t dht;
+    // Inicializacion de DHT usando el PIO0 para controlar el DATA_PIN
+    dht_init(&dht, DHT_MODEL, pio0, DATA_PIN, true /* pull_up */);
+    
+    while(1) {
+        // Inicio comunicacion con sensor
+        dht_start_measurement(&dht);
+        // Variables de humedad y temperatura
+        float humidity, temperature_c;
+        // Esperar a que la comunicacion termine y obtener los datos
+        dht_result_t result = dht_finish_measurement_blocking(&dht, &humidity, &temperature_c);
+        // Verificar el resultado de la operacion
+        if (result == DHT_RESULT_OK) {
+            // Mostrar temperatura y humedadhumidity
+            printf ("La humedad es de %.2f y la temperatura es de %.2f C o %.2f /n" ,humidity, temperature_c, celsius_to_fahrenheit(temperature_c))
+
+        } else if (result == DHT_RESULT_TIMEOUT) {
+            // Mensaje de error
+            puts("El DHT no responde, probablemente este mal el conexionado");
+        } else {
+            // Error de checksum
+            puts("Mal checksum");
+        }
+        // Demora para no saturar la consola
+        sleep_ms(500);
+    }
+}
